a4957d3435972a1f870dd86c62bc98b43abbaea8
manuelibarbia <manuelibarbia02@gmail.com>
2022-11-09 15:36:16 -0300

    ISwimmerRepository modificado (nuevos métodos), SwimmerService agregado

--- TPIntegradorProgIII/Data/Repository/Interfaces/ISwimmerRepository.cs
+++ TPIntegradorProgIII/Data/Repository/Interfaces/ISwimmerRepository.cs
@@ -5,5 +5,7 @@ namespace TPIntegradorProgIII.Data.Repository.Interfaces
     public interface ISwimmerRepository
     {
         Swimmer? GetSwimmerById(int userId);
+        ICollection<Meet> GetSwimmerMeets(int swimmerId);
+        ICollection<Trial> GetSwimmerTrials(int swimmerId);
     }
 }
---
+++ TPIntegradorProgIII/Services/Implementations/SwimmerService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using TPIntegradorProgIII.Data.Repository.Interfaces;
+using TPIntegradorProgIII.Models;
+using TPIntegradorProgIII.Services.Interfaces;
+
+namespace TPIntegradorProgIII.Services.Implementations
+{
+    public class SwimmerService : ISwimmerService
+    {
+        private readonly ISwimmerRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public SwimmerService(ISwimmerRepository swimmerRepository, IMapper mapper)
+        {
+            _userRepository = swimmerRepository;
+            _mapper = mapper;
+        }
+        public ICollection<MeetDto> GetMeetsBySwimmer(int swimmerId)
+        {
+            var meets = _userRepository.GetSwimmerMeets(swimmerId);
+
+            return _mapper.Map<ICollection<MeetDto>>(meets);
+        }
+
+        public ICollection<TrialDto> GetTrialsBySwimmer(int swimmerId)
+        {
+            var trials = _userRepository.GetSwimmerTrials(swimmerId);
+
+            return _mapper.Map<ICollection<TrialDto>>(trials);
+        }
+    }
+}
