e5700fe1f47e6201da7ab34470d95696c63a42ea
manuelibarbia <manuelibarbia02@gmail.com>
2022-11-09 12:48:28 -0300

    Agregada carpeta enums, no sé si la necesitamos

---
+++ TPIntegradorProgIII/Enums/QuestionState.cs
@@ -0,0 +1,10 @@
+﻿namespace TPIntegradorProgIII.Enums
+{
+    public enum QuestionState
+    {
+        WaitingProfessorAnwser,
+        WaitingStudentAnwser,
+        Resolved,
+        Canceled
+    }
+}
