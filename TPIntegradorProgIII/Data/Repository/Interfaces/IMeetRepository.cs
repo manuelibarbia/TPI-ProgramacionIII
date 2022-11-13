5335997e727a19a6ee5dacadabf2207329876286
manuelibarbia <manuelibarbia02@gmail.com>
2022-11-13 13:19:41 -0300

    Modificado nombre de método en meets

--- TPIntegradorProgIII/Data/Repository/Interfaces/IMeetRepository.cs
+++ TPIntegradorProgIII/Data/Repository/Interfaces/IMeetRepository.cs
@@ -7,7 +7,7 @@ namespace TPIntegradorProgIII.Data.Repository
     public interface IMeetRepository
     {
         public Meet? GetSingleMeet(int id);
-        public List<Meet> GetAllMeets();
+        public List<Meet> GetMeets();
         public void AddMeet(Meet newMeet);
         public void RemoveMeet(int id);
         public void EditMeetDate(int id, string newMeetDate);
