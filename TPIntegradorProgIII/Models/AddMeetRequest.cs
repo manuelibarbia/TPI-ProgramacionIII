8fe9e0089f05fd0be684c6f3f13e3f40618a1e1d
manuelibarbia <manuelibarbia02@gmail.com>
2022-11-21 18:45:30 -0300

    Eliminado id de meetRequest

--- TPIntegradorProgIII/Models/AddMeetRequest.cs
+++ TPIntegradorProgIII/Models/AddMeetRequest.cs
@@ -6,7 +6,6 @@ namespace TPIntegradorProgIII.Models
 {
     public class AddMeetRequest
     {
-        public int Id { get; set; }
         public string MeetName { get; set; }
         public string MeetDate { get; set; }
         public string MeetPlace { get; set; }
